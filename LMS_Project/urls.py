00aa3a955e7005e9cc14e2870195cac93e4e48e0
Maryam Lotfi <lotfimaryam1110@gmail.com>
2025-11-05 06:35:51 -0900

    save change

--- LMS_Project/urls.py
+++ LMS_Project/urls.py
@@ -1,21 +1,7 @@
-"""
-URL configuration for LMS_Project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -23,7 +9,24 @@ from rest_framework_simplejwt.views import (
 )
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
+#-----------------------------------------------
 
+def custom_404_view(request, exception):
+    return Response(
+        {"detail": "آدرس مورد نظر پیدا نشد. لطفاً آدرس را بررسی کنید.", "code": "not_found"},
+        status=status.HTTP_404_NOT_FOUND
+    )
+
+def custom_500_view(request):
+    return Response(
+        {"detail": "خطای داخلی سرور رخ داده است. لطفاً به پشتیبانی اطلاع دهید.", "code": "server_error"},
+        status=status.HTTP_500_INTERNAL_SERVER_ERROR
+    )
+
+handler404 = custom_404_view
+handler500 = custom_500_view
+
+#-------------------------------------------
 
 urlpatterns = [
     path('admin/', admin.site.urls),
